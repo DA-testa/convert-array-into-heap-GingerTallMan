598f63a533d614be8ec8b241e085b41d7d4a9e20
Artūrs Adrians Zaharovs - Ploriņš <118048111+GingerTallMan@users.noreply.github.com>
2023-03-19 18:05:17 +0000

    modified

--- build_heap.py
+++ build_heap.py
@@ -4,18 +4,31 @@
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(data,0,-1):
+        j = i
+        while j > 1:
+            if data[i-1]<data[j-1]:
+                data[i-1], data[j-1] = data[j-1], data[i-1]
+                swaps.append([j-1,i-1])
+                i = j
+            else:
+                i=0
     return swaps
 
 
-def main():
-    
+def main():   
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
+    temp=input()
+    if 'I'in(temp):
+        # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif 'F'in(temp):
+        with open('tests/' + input().replace('\r',''), mode="r") as fails:
+            n = int(fails.readline())
+            data = list(map(int, fails.readline().split()))
 
     # input from keyboard
     n = int(input())
